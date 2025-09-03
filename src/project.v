8056a8d209aec52f0dc9f5129a529fd944108658
tapeoutcutm <tapeout@cutm.ac.in>
2025-09-03 20:38:28 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,19 +1,23 @@
 `default_nettype none
 
-// just a stub to keep the Tiny Tapeout tools happy
+// Stub module for Tiny Tapeout (tt09, no analog pins)
 
 module tt_um_pll_delayenhance (
     input  wire       VGND,
     input  wire       VPWR,
     input  wire [7:0] ui_in,
-    output wire [7:0] uo_out,pll
+    output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
     output wire [7:0] uio_out,
     output wire [7:0] uio_oe,
-    inout  wire [7:0] ua, // analog pins
     input  wire       ena,
     input  wire       clk,
     input  wire       rst_n
 );
+
+    // Safe default assignments
+    assign uo_out  = 8'b0;
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
 endmodule
